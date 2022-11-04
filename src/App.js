182b62136efe7d23e5f595455ace73e1441d210f
Das_Babu <dass7550@gmail.com>
2022-11-04 12:11:50 +0600

    fetch and function cal, jsx created

--- src/App.js
+++ src/App.js
@@ -22,6 +22,19 @@ function LoadCountries() {
     <div>
       <h1> visiting rest of the countries</h1>
       <h3>available countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h3>Population: {props.population}</h3>
+
     </div>
   )
 }
